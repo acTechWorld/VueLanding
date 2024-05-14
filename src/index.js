fb8acd374f9c3ee5acf292565b11d213b1e860c7
oncekiller <canard.antoine@gmail.com>
2024-05-14 17:57:53 +0200

    prepare for export as lib

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ChatDisplay from '@/components/ChatDisplay.vue'
+import ChatBoxInput from '@/components/ChatBoxInput.vue'
+import ProgressBar from '@/components/ProgressBar.vue'
+
+export { ChatDisplay, ChatBoxInput, ProgressBar }
