a9c2f50a27f081cc1ccfcfd84fa90bfed588fce3
oncekiller <canard.antoine@gmail.com>
2024-05-14 18:29:15 +0200

    setup for npm package

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,21 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'MyComponentLibrary'
+    },
+    rollupOptions: {
+      // Make sure to externalize Vue to prevent it from being bundled
+      external: ['vue'],
+      output: {
+        // Provide global variables to use externally
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
   }
 })
