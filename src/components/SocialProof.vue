352f7ed7b1da4b256f1661ea9d3435949b07435d
oncekiller <canard.antoine@gmail.com>
2024-05-18 21:16:04 +0200

    ajout test SocialProof

--- src/components/SocialProof.vue
+++ src/components/SocialProof.vue
@@ -1,29 +1,41 @@
 <template>
-  <SwiperComponent :animation-type="props.animationType" :infinite="props.infinite">
+  <SwiperComponent
+    :animation-type="props.animationType"
+    :infinite="props.infinite"
+    data-test="socialProof"
+  >
     <div
       v-for="(data, key) in props.datas"
       :key="`proof_${key}`"
       :style="style"
       class="px-20 py-5 text-center gap-8 flex flex-col"
+      data-test="social-proof-item"
     >
-      <div v-if="data.testimony" class="text-4xl font-semibold">{{ data.testimony }}</div>
+      <div v-if="data.testimony" data-test="social-testimony" class="text-4xl font-semibold">
+        {{ data.testimony }}
+      </div>
       <div
         v-if="data.name || data.title || data.imgProfile || data.stars"
         class="items-center flex flex-col"
+        data-test="bottom-infos"
       >
         <img
           v-if="data.imgProfile"
           :src="data.imgProfile"
           alt="Profile Image"
           class="w-16 h-16 rounded-full mb-2 object-cover"
+          data-test="social-imgProfile"
         />
-        <p v-if="data.name" class="text-lg font-semibold">{{ data.name }}</p>
-        <p v-if="data.title">{{ data.title }}</p>
+        <p v-if="data.name" class="text-lg font-semibold" data-test="social-name">
+          {{ data.name }}
+        </p>
+        <p v-if="data.title" data-test="social-title">{{ data.title }}</p>
         <div v-if="data.stars" class="mt-2">
           <FontAwesomeIcon
             v-for="(icon, idx) in calculateStarsIcon(data.stars)"
             :key="`proof_${key}_star_${idx}`"
             :icon="icon"
+            data-test="social-star"
           />
         </div>
       </div>
