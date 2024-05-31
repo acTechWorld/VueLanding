6735dcfca98473d22a7ce4f5cfcb6766ce4703ee
oncekiller <canard.antoine@gmail.com>
2024-05-31 20:22:17 +0200

    add new component NewsletterSection + test

---
+++ src/components/NewsletterSection.stories.js
@@ -0,0 +1,28 @@
+import NewsletterSection from './NewsletterSection.vue'
+
+export default {
+  title: 'Components/Newsletter Section',
+  component: NewsletterSection
+}
+
+const Template = (args) => ({
+  components: { NewsletterSection },
+  setup: () => ({ args }),
+  template: '<NewsletterSection v-bind="args" class="max-w-[1400px]"  />'
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  title: 'Sign up for our newsletter',
+  subTitle: 'Be the first to know about releases and industry news and insights',
+  img: 'https://creatoom.com/wp-content/uploads/2023/12/scene-with-box-paper-mockups-v4-top-view-1024x683.jpg',
+  placeholderInput: 'Enter your email',
+  ctaButton: {
+    name: 'subscribe',
+    label: 'Subscribe',
+    themeColor: 'tertiary'
+  },
+  bgColor: undefined,
+  color: undefined,
+  themeColor: undefined
+}
---
+++ src/components/NewsletterSection.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="newsletterSection md:flex" :style="sectionStyle">
+    <div
+      :class="{ 'md:w-1/2 md:gap-5 lg:gap-10 md:py-10 md:text-left': props.img }"
+      class="newsletterSection_left flex flex-col gap-10 justify-center px-12 py-20 text-center w-full"
+    >
+      <div v-if="props.title" class="newsletterSection_title text-4xl font-semibold">
+        {{ props.title }}
+      </div>
+      <div v-if="props.subTitle" class="newsletterSection_subTitle">{{ props.subTitle }}</div>
+      <div class="newsletterSection_emailCtn gap-2" :class="{ 'md:flex md:flex-wrap': props.img }">
+        <input
+          v-model="email"
+          :placeholder="props.placeholderInput"
+          class="newsletterSection_emailCtn_input px-4 py-2 border rounded-lg w-full max-w-[400px] text-[#000]"
+        />
+        <CTAButton
+          v-bind="props.ctaButton"
+          class="newsletterSection_emailCtn_ctaButton mx-auto mt-5"
+          :class="{ 'md:mx-0 md:mt-0': props.img }"
+          @click="handleSubmit"
+        />
+      </div>
+    </div>
+    <div v-if="props.img" class="newsletterSection_right w-full md:w-1/2">
+      <img :src="props.img" class="newsletterSection_img h-full object-cover" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CTAButton from '@/commons/CTAButton.vue'
+import type { ThemeColor, CTAButtonType } from '@/utils/types'
+import { getBgColor, getTxtColor } from '@/utils/utils'
+import { ref, computed } from 'vue'
+const props = withDefaults(
+  defineProps<{
+    title?: string
+    subTitle?: string
+    img?: string
+    placeholderInput?: string
+    ctaButton?: CTAButtonType
+    bgColor?: `#${string}`
+    color?: `#${string}`
+    themeColor?: ThemeColor
+  }>(),
+  {
+    title: undefined,
+    subTitle: undefined,
+    img: undefined,
+    placeholderInput: undefined,
+    ctaButton: () => ({
+      name: 'subscribe',
+      label: 'Subscribe'
+    }),
+    bgColor: undefined,
+    color: undefined,
+    themeColor: undefined
+  }
+)
+
+const email = ref()
+const emits = defineEmits(["submit"])
+
+/** COMPUTED */
+const sectionStyle = computed(() => ({
+  color: getTxtColor(props.color, props.themeColor),
+  backgroundColor: getBgColor(props.bgColor, props.themeColor)
+}))
+
+/** METHODS */
+const handleSubmit = () => emits('submit', email.value)
+</script>
