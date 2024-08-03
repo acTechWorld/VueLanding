a4a60d2db2135d3f3cfc821a0e94f345e2bf947c
oncekiller <canard.antoine@gmail.com>
2024-08-03 22:52:45 +0200

    fix backend url

--- src/services/licencing.js
+++ src/services/licencing.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export async function validateLicenseKey(key) {
   try {
     const response = await axios.get(
-      `http://fule0038.odns.fr/api/licencing/validate-key?licencingKey=${key}&project=vueLanding`
+      `https://utils-backend.vuelanding.com/api/licencing/validate-key?licencingKey=${key}&project=vueLanding`
     )
     return response.data
   } catch (error) {
